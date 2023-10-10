3c878acb2def363e8c99c427a46c2e28177bad3c
Nghia Pham <nghia.pham@docusign.com>
2023-10-10 10:06:33 +0100

    - add data_dir parameters in bash, change torch version

--- exec_container.sh
+++ exec_container.sh
@@ -1,7 +1,8 @@
-export G=$1
-export GPU=$2
-export WAY=$3
-export SHOT=$4
+export DATA=$1
+export G=$2
+export GPU=$3
+export WAY=$4
+export SHOT=$5
 export way=${WAY}
 export shot=${SHOT}
 echo $shot $way
@@ -17,5 +18,5 @@ export is_viterbi=viterbi
 for ((i=0;i<50;i++))
   do
   echo $shot $way $i
-  python src/container.py --data_dir data/few-nerd/${G} --labels-train data/few-nerd/${G}/labels_train.txt --labels-test data/few-nerd/${G}/labels_test.txt --config_name bert-base-uncased --model_name_or_path bert-base-uncased --saved_model_dir saved_models/few-nerd/${G}/${SAVED_MODEL_DIR} --output_dir outputs/few-nerd/${G}/${finetune_loss}_${is_viterbi}_final/${G}-${way}-${shot}/${i} --support_path support_test_${way}_${shot}/${i} --test_path query_test_${way}_${shot}/${i} --n_shots ${shot} --max_seq_length 128 --embedding_dimension 128 --num_train_epochs 1 --train_batch_size 32 --seed 1 --do_predict --select_gpu ${GPU} --training_loss KL --finetune_loss ${finetune_loss} --evaluation_criteria euclidean_hidden_state --learning_rate 5e-5 --learning_rate_finetuning 5e-5 --consider_mutual_O --temp_trans 0.01 --silent
+  python src/container.py --data_dir ${DATA}/few-nerd/${G} --labels-train ${DATA}/few-nerd/${G}/labels_train.txt --labels-test ${DATA}/few-nerd/${G}/labels_test.txt --config_name bert-base-uncased --model_name_or_path bert-base-uncased --saved_model_dir saved_models/few-nerd/${G}/${SAVED_MODEL_DIR} --output_dir outputs/few-nerd/${G}/${finetune_loss}_${is_viterbi}_final/${G}-${way}-${shot}/${i} --support_path support_test_${way}_${shot}/${i} --test_path query_test_${way}_${shot}/${i} --n_shots ${shot} --max_seq_length 128 --embedding_dimension 128 --num_train_epochs 1 --train_batch_size 32 --seed 1 --do_predict --select_gpu ${GPU} --training_loss KL --finetune_loss ${finetune_loss} --evaluation_criteria euclidean_hidden_state --learning_rate 5e-5 --learning_rate_finetuning 5e-5 --consider_mutual_O --temp_trans 0.01 --silent
   done
